91470b8a9224031b3e5eefcc2198e36244a092c6
Yoongundam <gun4336@naver.com>
2023-09-27 15:05:56 +0900

    0927_1

---
+++ NW/3_week/Initwinsock/Initwinsock.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+
+void err_quit(const char* msg) {
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL
+	);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+int main(int argc, const char* argv[]) {
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+	MessageBox(NULL, "윈속 초기화 성공", "알림", MB_OK);
+
+	SOCKET tcp_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (tcp_sock == INVALID_SOCKET) err_quit("socket()");
+	MessageBox(NULL, "TCP 소켓 생성 성공", "알림", MB_OK);
+
+	closesocket(tcp_sock);
+
+	WSACleanup();
+
+	return 0;
+}
